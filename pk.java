5164ffaea3f5ab6839147f2427bec19b0924905f
Lukman143 <sklukman98@gmail.com>
2022-07-28 18:28:54 +0530

    commited by rahul

---
+++ pk.java
@@ -0,0 +1,26 @@
+package com.sk;
+
+import java.io.File;
+import org.alicebot.ab.*;
+import org.alicebot.ab.utils.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ChatBot1Application {
+
+	private static final boolean TRACE_MODE = sk;
+	int i=10;
+	
+	
+	}
+
+	private static String getpath() {
+		File currd = new File(".");
+		String path = currd.getAbsolutePath();
+		String resourcepath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+		return resourcepath;
+	}
+}
